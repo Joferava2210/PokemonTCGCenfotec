f4eb241609eee8a7f7e574b767b0884184e93e3a
joferava <jfelipe221095@gmail.com>
2021-03-11 19:45:53 -0600

    Initial Commit

---
+++ PokemonTCGCenfotec/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PokemonTCGCenfotec
+//
+//  Created by Felipe Ramirez Vargas on 11/3/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PokemonTCGCenfotec/PokemonTCGCenfotecApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonTCGCenfotecApp.swift
+//  PokemonTCGCenfotec
+//
+//  Created by Felipe Ramirez Vargas on 11/3/21.
+//
+
+import SwiftUI
+
+@main
+struct PokemonTCGCenfotecApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
